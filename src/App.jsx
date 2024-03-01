0127672f171e646415055ea341ba2d1433142d78
macluiggy <luiggy.macias@imagineapps.co>
2024-03-01 12:39:35 -0500

    Commented out unused import statement

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import {
-  FaCheckSquare as FullCheckIcon,
+  // FaCheckSquare as FullCheckIcon,
   FaRegCheckSquare as CheckIcon,
   FaRegComment as CommentIcon,
 } from "react-icons/fa";

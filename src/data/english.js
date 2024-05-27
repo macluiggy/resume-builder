a33c572dca33622544822101c28aa1833204ec81
macluiggy <luiggy.macias@imagineapps.co>
2024-05-27 10:05:04 -0500

    refactor: Update social media app description

--- src/data/english.js
+++ src/data/english.js
@@ -292,7 +292,7 @@ const data = {
          * red social full stack que permite a los usuarios crear una cuenta, hacer publicaciones, seguir a otros usuarios, ver el perfil de otros usuarios, etc. Para la autenticación se utilizó JWT y para la autorización se utilizó un middleware que verifica si el usuario tiene permisos para realizar ciertas acciones.
          * Cuenta con un backend desarrollado con NestJS y una base de datos PostgreSQL. El frontend está desarrollado con Angular y utiliza TypeScript. La aplicación se despliega utilizando Docker y se prueban las funcionalidades con Vitest.
          */
-        description: `A full stack social media app that allows users to create an account, make posts, follow other users, view other users' profiles, etc. JWT was used for authentication and a middleware was used for authorization to verify if the user has permissions to perform certain actions. The backend is developed with NestJS and a PostgreSQL database. The frontend is developed with Angular and uses TypeScript. The application is deployed using Docker and the functionalities are tested with Vitest.`,
+        description: `A full stack social media app that allows users to create an account, make posts, follow other users, view other users' profiles, etc. JWT was used for authentication and a middleware was used for authorization to verify if the user has permissions to perform certain actions.`,
         url: "https://staging-social-media.pages.dev/home",
       },
     ],
